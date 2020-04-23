25290015235939686b878baadac8944097c37219
Marcin Moch <marcio.moch@gmail.com>
2020-04-23 22:25:10 +0200

    Safeargs

--- app/src/main/java/mmoch/scrooge/debts_list/DebtsListFragment.kt
+++ app/src/main/java/mmoch/scrooge/debts_list/DebtsListFragment.kt
@@ -1,6 +1,7 @@
 package mmoch.scrooge.debts_list
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -20,8 +21,10 @@ class DebtsListFragment : Fragment() {
             DataBindingUtil.inflate(inflater,
                 R.layout.fragment_debts_list, container, false)
 
+        val action = DebtsListFragmentDirections.actionDebtsListFragmentToCreateEditDebtFragment(1)
+
         binding.buttonNext.setOnClickListener{
-            findNavController().navigate(R.id.action_debtsListFragment_to_createEditDebtFragment)
+            findNavController().navigate(action)
         }
 
         return binding.root
--- app/src/main/java/mmoch/scrooge/pay_off_simulator/PayOffSimulatorFragment.kt
+++ app/src/main/java/mmoch/scrooge/pay_off_simulator/PayOffSimulatorFragment.kt
@@ -9,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import mmoch.scrooge.R
 import mmoch.scrooge.databinding.FragmentPayOffSimulatorBinding
+import mmoch.scrooge.debts_list.DebtsListFragmentDirections
 
 class PayOffSimulatorFragment : Fragment() {
 
@@ -20,8 +21,10 @@ class PayOffSimulatorFragment : Fragment() {
             DataBindingUtil.inflate(inflater,
                 R.layout.fragment_pay_off_simulator, container, false)
 
+        val action = PayOffSimulatorFragmentDirections.actionPayOffSimulatorFragmentToCreateEditDebtFragment(2)
+
         binding.buttonPrev.setOnClickListener{
-            findNavController().navigate(R.id.action_payOffSimulatorFragment_to_createEditDebtFragment)
+            findNavController().navigate(action)
         }
 
         return binding.root
